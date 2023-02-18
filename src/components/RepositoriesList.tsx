c1a0eeb5f88d74522e269bb1310caf70c22670e0
Robson Carvalho <robson01jc@gmail.com>
2023-02-18 12:22:23 -0300

    finished app

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,19 @@
 import { useState } from 'react';
+import { useActions } from '../hooks/useActions';
+import { useTypedSelector } from '../hooks/useTypedSelector';
+import { RootState } from '../state';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
+  const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state: RootState) => state.repositories
+  );
 
   const onSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
+
+    searchRepositories(term);
   };
 
   return (
@@ -13,6 +22,9 @@ const RepositoriesList: React.FC = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
